094db62a316ae0de2370f56609f9ac6f5cc22866
ivanm93 <cristianmonzon91@gmail.com>
2026-06-22 11:19:14 -0300

    reparacion del envio del mai

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,14 +1,14 @@
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  family: 4, // fuerza IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: true,
-  }
 })
 
 export async function sendVerificationEmail(to, token) {
