c1f3ce20895ed4fe8e2544c84ae7a470a6fc77eb
ivanm93 <cristianmonzon91@gmail.com>
2026-06-26 21:58:55 -0300

    foto de perfil agregado

--- src/models/User.js
+++ src/models/User.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema(
         type: String,
         required: true
     },
+        profilePicture: {
+        type: String,
+        default: ''
+    },
     followers: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
